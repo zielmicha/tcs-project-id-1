8823dbf33cb41d96474757b8cace566180ce76ff
butataatawa <jan.derbisz@gmail.com>
2015-06-01 15:01:04 +0200

    Update Informacje_Medyczne.sql

--- Informacje_Medyczne.sql
+++ Informacje_Medyczne.sql
@@ -1,5 +1,14 @@
-CREATE TABLE pacjenci(
+CREATE TABLE osoby(
 id int primary key,
 imie varchar(50) not null,
 nazwisko varchar(50) not null
+pesel char(11) not null
+);
+CREATE TABLE lekarze(
+id int primary key
+id_osoby int references osoby(id)
+);
+CREATE TABLE specjalizacje(
+id int primary key,
+id_lekarza int references lekarze(id)
 );
