ef65a9b0e45cbe106956463e4878fd2c78870495
butataatawa <jan.derbisz@gmail.com>
2015-06-04 13:58:02 +0200

    Update Informacje_Medyczne.sql

--- Informacje_Medyczne.sql
+++ Informacje_Medyczne.sql
@@ -2,7 +2,7 @@ CREATE TABLE osoby(
 id int primary key,
 imie varchar(50) not null,
 nazwisko varchar(50) not null
-pesel char(11) not null
+pesel char(11)
 );
 CREATE TABLE lekarze(
 id int primary key
@@ -11,4 +11,38 @@ id_osoby int references osoby(id)
 CREATE TABLE specjalizacje(
 id int primary key,
 id_lekarza int references lekarze(id)
+);
+CREATE TABLE uslugodawcy(
+id int primary key,
+nazwa varchar(150) not null,
+adres varchar(150) not null
+);
+CREATE TABLE uslugi(
+id int primary key,
+id_lekarza references lekarze(id),
+id_osoby references osoby(id),
+id_uslugodawcy references uslugodawcy(id)
+);
+CREATE TABLE recepty(
+id int primary key,
+id_lekarza references lekarze(id),
+id_osoby references osoby(id)
+);
+CREATE TABLE apteki(
+id int primary key,
+nazwa varchar(150) not null,
+adres varchar(150) not null,
+id_oddzialu references oddzialy(id)
+);
+CREATE TABLE oddzialy(
+id int primary key,
+nazwa varchar(150) not null,
+adres varchar(150) not null
+);
+CREATE TABLE umowy(
+id int primary key,
+id_oddzialu references oddzialy(id),
+id_uslugodawcy references uslugodawcy(id),
+od date not null,
+do date not null
 );
