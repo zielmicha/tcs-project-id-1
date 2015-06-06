1e231f8f018b398ba4da2a0d8287e831037d8997
butataatawa <jan.derbisz@gmail.com>
2015-06-06 19:44:15 +0200

    Update Informacje_Medyczne.sql

--- Informacje_Medyczne.sql
+++ Informacje_Medyczne.sql
@@ -47,3 +47,21 @@ data_od date not null,
 data_do date not null,
 CONSTRAINT daty data_od<=data_do
 );
+CREATE OR REPLACE FUNCTION check_pesel() RETURNS trigger AS $check_pesel$
+DECLARE
+b integer;
+BEGIN
+IF char_length(NEW.pesel)!=11 then
+      RAISE EXCEPTION 'Niepoprawny PESEL';
+END IF;
+b=(ascii(substring(NEW.pesel from 1 for 1))-48)+3*(ascii(substring(NEW.pesel from 2 for 1))-48)+7*(ascii(substring(NEW.pesel from 3 for 1))-48)+9*(ascii(substring(NEW.pesel from 4 for 1))-48)+(ascii(substring(NEW.pesel from 5 for 1))-48)+3*(ascii(substring(NEW.pesel from 6 for 1))-48)+7*(ascii(substring(NEW.pesel from 7 for 1))-48)+9*(ascii(substring(NEW.pesel from 8 for 1))-48)+(ascii(substring(NEW.pesel from 9 for 1))-48)+3*(ascii(substring(NEW.pesel from 10 for 1))-48);
+b=b%10;
+b=(10-b)%10;
+IF b!=(ascii(substring(NEW.pesel from 11 for 1))-48) then
+RAISE EXCEPTION 'Niepoprawny PESEL';
+END IF;
+RETURN NEW;
+END;
+$check_pesel$ LANGUAGE plpgsql;
+CREATE TRIGGER check_pesel BEFORE INSERT OR UPDATE ON osoby
+FOR EACH ROW EXECUTE PROCEDURE check_pesel();
