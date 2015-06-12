48f879d8caa26a8a272411ab0db8e7995c4dd833
Franciszek Stokowacki <franek.stokowacki@gmail.com>
2015-06-12 20:20:24 +0200

    new views, more not-null declarations

--- clear.sql
+++ clear.sql
@@ -1,22 +1,19 @@
 drop table if exists osoby cascade;
-drop table if exists lekarze cascade;
-drop table if exists specjalizacje cascade;
 drop table if exists uslugodawcy cascade;
+drop table if exists lekarze cascade;
+drop table if exists zatrudnieni cascade;
+drop table if exists specjalizacje cascade;
 drop table if exists typy_uslug cascade;
 drop table if exists uslugi cascade;
 drop table if exists oddzialy cascade;
 drop table if exists apteki cascade;
 drop table if exists recepty cascade;
 drop table if exists leki cascade;
+drop table if exists choroby cascade;
 drop table if exists recepta_lek cascade;
 drop table if exists zgloszenie cascade;
 drop table if exists umowy cascade;
-drop table if exists zatrudnieni cascade;
-drop table if exists choroby cascade;
-
-drop function if exists czy_ma_umowe(placowka bigint, kiedy timestamp) cascade;
-drop function if exists czy_ubezpieczony(czlowiek int, kiedy timestamp) cascade;
+drop table if exists historia_chorob cascade;
+drop function if exists czy_ubezpieczony (czlowiek int, kiedy timestamp ) cascade;
+drop function if exists czy_ma_umowe (placowka bigint, kiedy timestamp) cascade;
 drop function if exists pesel_trigger() cascade;
-
-drop view if exists lekarze_dane;
-drop view if exists recepty_koszt;
